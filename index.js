18b5032592e1d6791b4fd60f051d14cf840452d4
Harold Vargas <harold.vargas@bewe.io>
2020-03-28 20:58:52 -0500

    nodemon local

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app = require('./server')
 
-const MongoClient = require('mongodb').MongoClient
+app.listen(app.get('port'), () =>{
+  console.log('server on port 3000')
+})
+// const app = require('./server')
+// const port = 3000
 
-// Connection URL
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
+// const MongoClient = require('mongodb').MongoClient
 
-app.get('/', (req, res) => {
-  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
-    if (err) {
-      res.status(500).send('💥 BOOM  error 💥: ' + err);
-    } else {
-      res.send('Me conecté a la DB! 😎 probando docker');
-      db.close();
-    }
-  });
-});
+// // Connection URL
+// const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 
-app.listen(port, () => console.log(`servidor  on port ${port}!`))
+// app.get('/', (req, res) => {
+//   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
+//     if (err) {
+//       res.status(500).send('💥 BOOM  error 💥: ' + err);
+//     } else {
+//       res.send('Me conecté a la DB! 😎 probando docker');
+//       db.close();
+//     }
+//   });
+// });
+
+// app.listen(port, () => console.log(`servidor  on port ${port}!`))
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+//inicializacion
+const app = express()
+
+
+//settings
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join( __dirname + '/views'))
+
+//midelwares
+app.use(express.urlencoded({extended: false}))
+//vglobal variables
+
+//routers
+app.get('/', (req, res) => {
+    res.send('hello word')
+})
+//static file g
+
+module.exports = app
